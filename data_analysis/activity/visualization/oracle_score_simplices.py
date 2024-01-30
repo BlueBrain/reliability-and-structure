a72b8e197e696180c0dc705503a66912e409ad54
Ecker András <andras.ecker@epfl.ch>
2024-01-30 17:13:53 +0100

    added restriction to L234

--- data_analysis/activity/visualization/oracle_score_simplices.py
+++ data_analysis/activity/visualization/oracle_score_simplices.py
@@ -23,6 +23,8 @@ sns.set(style="ticks", context="paper", font="Helvetica Neue",
             "ytick.major.size": 2, "ytick.major.width": 0.5, "ytick.minor.size": 1.5, "ytick.minor.width": 0.3})
 STRUCTURAL_DATA_DIR = "/gpfs/bbp.cscs.ch/home/egassant/reliability_and_structure/data_analysis/data"
 FUNCTIONAL_DATA_DIR = "/gpfs/bbp.cscs.ch/project/proj96/home/ecker/assemblyfire/MICrONS"
+L234_MTYPES = ["23P", "4P"]
+L56_MTYPES = ["5P_IT", "5P_NP", "5P_PT", "6IT", "6CT"]
 COLORS = {"all": "tab:orange", "sink": "tab:green", "source": "tab:blue"}
 MARKERS = {"all": "o", "sink": ">", "source":"<"}
 
@@ -34,21 +36,38 @@ def load_simplex_list(maximal=True):
 
 
 def load_functional_data(npzf_name, conn_mat):
-    """Loads oracle score from MICrONS functional data (saved to npz in `assemblyfire`),
-    maps idx to the structural connectome, and padds it with nans to have the same shape as the structural data"""
+    """Loads ''spikes'' and oracle score from MICrONS functional data (saved to npz in `assemblyfire`),
+    maps idx to the structural connectome, and padds oracle score it with nans to have the same shape
+    as the structural data"""
     tmp = np.load(npzf_name)
-    oracle_scores = pd.Series(tmp["oracle_scores"], index=tmp["idx"], name="oracle score")
+    idx, spikes = tmp["idx"], tmp["spikes"]
+    oracle_scores = pd.Series(tmp["oracle_scores"], index=idx, name="oracle score")
     # match idx to structural data
     valid_idx = conn_mat.vertices.id[np.isin(conn_mat.vertices.id, oracle_scores.index)]
     oracle_scores = oracle_scores.loc[oracle_scores.index.isin(valid_idx)]
     valid_idx_tmp = pd.Series(valid_idx.index.to_numpy(), index=valid_idx.to_numpy())
     oracle_scores = pd.Series(oracle_scores.to_numpy(), index=valid_idx_tmp.loc[oracle_scores.index].to_numpy(),
                               name="oracle score").sort_index()
-    # padd with NaNs
+    # index out spikes as well (and sort gids)
+    idx_tmp = np.where(np.in1d(idx, valid_idx_tmp.index.to_numpy()))[0]
+    gids = valid_idx_tmp.loc[idx[idx_tmp]].to_numpy()
+    sort_idx = np.argsort(gids)
+    spikes = spikes[idx_tmp[sort_idx], :]
+    # padd oracle scores with NaNs
     idx = conn_mat.vertices.index.to_numpy()
     data = np.full(len(idx), np.nan)
     data[oracle_scores.index.to_numpy()] = oracle_scores.to_numpy()
-    return pd.Series(data, index=idx)
+    return spikes, gids[sort_idx], pd.Series(data, index=idx)
+
+
+def population_coupling(binned_spikes):
+    """Fast implementation of population coupling (aka. coupling coefficient: corr of binned spikes
+    with the avg. of the binned spikes of the rest of neurons') by Michael"""
+    binned_spikes = binned_spikes - binned_spikes.mean(axis=1, keepdims=True)
+    mn_pop = binned_spikes.mean(axis=0)
+    A = np.dot(binned_spikes, mn_pop.reshape((-1, 1)))[:, 0]
+    B = np.sqrt(np.var(mn_pop) * np.var(binned_spikes, axis=1)) * binned_spikes.shape[1]
+    return A / B
 
 
 def agg_along_dims(stats):
@@ -90,8 +109,11 @@ def plot_y_vs_sdim_summary(dict, ylabel, fig_name):
     fig = plt.figure(figsize=(1.8, 1.6))
     ax = fig.add_subplot(1, 1, 1)
     x = np.arange(1, 8)  # pretty hard coded...
-    for label, y in dict.items():
+    ys = np.full((len(dict), len(x)), np.nan)
+    for i, (label, y) in enumerate(dict.items()):
         ax.plot(x, y, label=label)
+        ys[i, :] = y
+    ax.plot(x, np.nanmean(ys, axis=0), color="black", label="mean of all scans")
     ax.set_xticks(x)
     ax.set_xlabel("Simplex dimension")
     ax.set_ylabel(ylabel)
@@ -101,26 +123,47 @@ def plot_y_vs_sdim_summary(dict, ylabel, fig_name):
     plt.close(fig)
 
 
+def main(conn_mat, session_idx, scan_idx, maximal=False, only_l234=False, n_ctrl=10):
+    max_str = "_max_" if maximal else "_"
+    l234_str = "_L234_" if only_l234 else "_"
+    simplices = load_simplex_list(maximal=maximal)
+    node_part = topology.node_participation(conn_mat.matrix, max_simplices=maximal, threads=8)
+    if only_l234:
+        node_part.loc[conn_mat.vertices["cell_type"].isin(L56_MTYPES), :] = 0
+    all_node_part_sums = node_part.sum()
+
+    sum_stats, frac_node_parts = {}, {}
+    for session_id, scan_id in zip(session_idx, scan_idx):
+        name_tag = "MICrONS_session%i_scan%i" % (session_id, scan_id)
+        spikes, gids, functional_data = load_functional_data(os.path.join(FUNCTIONAL_DATA_DIR, "%s.npz" % name_tag), conn_mat)
+        cc = population_coupling(spikes)
+        # TODO: cont. from here
+        if only_l234:
+            mtypes = conn_mat.vertices.loc[functional_data.loc[functional_data.notna()].index, "cell_type"]
+            functional_data.loc[mtypes.loc[mtypes.isin(L56_MTYPES)].index] = np.nan
+        stats = agg_along_dims(nstats.node_stats_per_position(simplices, functional_data,
+                                                              dims=simplices.index.drop(0), with_multiplicity=True))
+        sum_stats[name_tag[8:]] = stats["all"]["mean"].to_numpy()
+        node_part_sums = node_part.loc[functional_data.notna()].sum()
+        frac_node_parts[name_tag[8:]] = node_part_sums.loc[1:].to_numpy() / all_node_part_sums.loc[1:].to_numpy()
+        plot_oracle_scores_vs_sdim(stats, node_part_sums,
+                                   "figs/%s_oracle_score%svs%ssimplex_dim.pdf" % (name_tag, l234_str, max_str))
+    plot_y_vs_sdim_summary(sum_stats, "Mean oracle score",
+                           "figs/MICrONS_oracle_score%svs%ssimplex_dim.pdf" % (l234_str, max_str))
+    plot_y_vs_sdim_summary(frac_node_parts, "Node participation ratio",
+                           "figs/MICrONS_node_part_sum%svs%ssimplex_dim.pdf" % (l234_str, max_str))
+
+
 if __name__ == "__main__":
+    # sessions 8, 5 are the ones that we usually discard
+    session_idx, scan_idx = [4, 5, 6, 6, 6, 7, 8, 9], [7, 7, 2, 4, 7, 4, 5, 3]
     conn_mat = load_connectome(STRUCTURAL_DATA_DIR, "MICrONS")
-    for maximal, plt_str in zip([True, False], ["_max_", "_"]):
-        simplices = load_simplex_list(maximal=maximal)
-        node_part = topology.node_participation(conn_mat.matrix, max_simplices=maximal, threads=8)
-        all_node_part_sums = node_part.sum()
-        sum_stats, frac_node_parts = {}, {}
-        for session_id, scan_id in zip([4, 5, 6, 6, 6, 7], [7, 7, 2, 4, 7, 4]):
-            name_tag = "MICrONS_session%i_scan%i" % (session_id, scan_id)
-            functional_data = load_functional_data(os.path.join(FUNCTIONAL_DATA_DIR, "%s.npz" % name_tag), conn_mat)
-            stats = agg_along_dims(nstats.node_stats_per_position(simplices, functional_data,
-                                                                  dims=simplices.index.drop(0), with_multiplicity=True))
-            sum_stats[name_tag[8:]] = stats["all"]["mean"].to_numpy()
-            node_part_sums = node_part.loc[functional_data.notna()].sum()
-            frac_node_parts[name_tag[8:]] = node_part_sums.loc[1:].to_numpy() / all_node_part_sums.loc[1:].to_numpy()
-            plot_oracle_scores_vs_sdim(stats, node_part_sums,
-                                       "figs/%s_oracle_score_vs%ssimplex_dim.pdf" % (name_tag, plt_str))
-        plot_y_vs_sdim_summary(sum_stats, "Mean oracle score", "figs/MICrONS_oracle_score_vs%ssimplex_dim.pdf" % plt_str)
-        plot_y_vs_sdim_summary(frac_node_parts, "Node participation ratio",
-                               "figs/MICrONS_node_part_sum_vs%ssimplex_dim.pdf" % plt_str)
+    main(conn_mat, session_idx, scan_idx, maximal=True, only_l234=True)
+    main(conn_mat, session_idx, scan_idx, maximal=True, only_l234=False)
+    main(conn_mat, session_idx, scan_idx, maximal=False, only_l234=True)
+    main(conn_mat, session_idx, scan_idx, maximal=False, only_l234=False)
+
+
 
 
 
