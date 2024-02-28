a3d2bb9d48f66942c337969cd05dfb3382be2e39
Ecker András <andras.ecker@epfl.ch>
2024-02-28 14:38:17 +0100

    BBP raster added

--- data_analysis/activity/visualization/rasters.py
+++ data_analysis/activity/visualization/rasters.py
@@ -1,9 +1,10 @@
 """
-...
+Activity plots
 author: András Ecker, last update: 02.2024
 """
 
 import os
+import json
 import numpy as np
 import pandas as pd
 from bluepy import Simulation
@@ -21,6 +22,9 @@ sns.set(style="ticks", context="paper", font="Helvetica Neue",
 RED, BLUE = "#e32b14", "#3271b8"
 MICRONS_FN_DATA_DIR = "/gpfs/bbp.cscs.ch/project/proj96/home/ecker/assemblyfire/MICrONS"
 BBP_FN_DATA_DIR = "/gpfs/bbp.cscs.ch/data/scratch/proj9/bisimplices/bbp_workflow/7b381e96-91ac-4ddd-887b-1f563872bd1c"
+PATTERNS = ["A", "B", "C", "D", "E", "F", "G", "H"]
+CMAP = plt.cm.get_cmap("tab10", len(PATTERNS))
+PATTERN_COLORS = {pattern: matplotlib.colors.to_hex(CMAP(i)) for i, pattern in enumerate(PATTERNS)}
 
 
 def _calc_rate(spike_times, n, t_start, t_end, bin_size=10):
@@ -58,30 +62,95 @@ def setup_raster(sim, t_start, t_end):
     rates = {syn_class: _calc_rate(spike_times[cols == color], ns[syn_class], t_start, t_end)
              for syn_class, color in zip(["EXC", "INH"], [RED, BLUE])}
 
-    return spike_times, spiking_ys, cols, rates, ylim, yticks, yticklabels
+    return spike_times - t_start, spiking_ys, cols, rates, [0, t_end - t_start], ylim, yticks, yticklabels
 
 
-def plot_raster(spike_times, spiking_ys, cols, rates, ylim, yticks, yticklabels, xlim, fig_name):
-    """..."""
-    fig = plt.figure(figsize=(8, 4))
+def setup_input_raster(sim, t_start, t_end):
+    """Organize gids by rate and get stimulus train"""
+    # spikes
+    input_fname = os.path.abspath(os.path.join(BBP_FN_DATA_DIR, "0", sim.config["Stimulus_spikeReplay"]["SpikeFile"]))
+    spikes = pd.read_csv(input_fname, sep="\t").reset_index()
+    spikes.rename(columns={"index": "t", "/scatter": "gid"}, inplace=True)
+    spikes = spikes.loc[(t_start <= spikes["t"]) & (spikes["t"] <= t_end)]
+    spike_times = spikes["t"].to_numpy()
+    n_spikes = spikes["gid"].value_counts().to_frame()
+    n_spikes["new_gid"] = np.arange(len(n_spikes))
+    spikes["new_gid"] = n_spikes.loc[spikes["gid"], "new_gid"].to_numpy()
+    rate = _calc_rate(spike_times, len(n_spikes), t_start, t_end)
+    # stimulus train and fiber locs
+    input_config = os.path.splitext(input_fname)[0] + ".json"
+    with open(input_config, "r") as f:
+        stim_config = json.load(f)
+    stims = pd.DataFrame(np.array(stim_config["props"]["time_windows"][:-1]), columns=["t"])
+    stims["id"] = np.array(stim_config["props"]["stim_train"])
+    stims = stims.loc[(t_start <= stims["t"]) & (stims["t"] < t_end)]
+    stims["name"] = stims["id"].map({i: name for i, name in enumerate(PATTERNS)})
+
+    return spike_times - t_start, spikes["new_gid"].to_numpy(), rate, [0, t_end - t_start], [-1, len(n_spikes)], \
+           stims["t"].to_numpy() - t_start, stims["name"].to_numpy(), stim_config["cfg"]["duration_stim"], \
+           stim_config["props"]["pattern_grps"], np.array(stim_config["props"]["grp_pos"])
+
+
+def plot_raster(spike_times, spiking_ys, cols, rates, xlim, ylim, yticks, yticklabels, fig_dir):
+    """Raster and firing rates"""
+    t_rate = np.linspace(xlim[0], xlim[1], len(rates["EXC"]))
+    fig = plt.figure(figsize=(8, 2))
     ax = fig.add_subplot(1, 1, 1)
     ax.set_facecolor((0.95, 0.95, 0.95))
-    ax.scatter(spike_times, spiking_ys, c=cols, alpha=0.9, marker='.', s=1, edgecolor="none")
-
-    t_rate = np.linspace(t_start, t_end, len(rates["EXC"]))
-
+    ax.scatter(spike_times, spiking_ys, c=cols, alpha=0.9, marker='.', s=0.5, edgecolor="none")
+    ax2 = ax.twinx()
+    ax2.plot(t_rate, rates["EXC"], RED)
+    ax2.plot(t_rate, rates["INH"], BLUE)
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
     ax.set_yticks(yticks)
+    ax2.set_ylim(bottom=0)
     ax.set_yticklabels(yticklabels)
     fig.savefig(fig_name, bbox_inches="tight", transparent=True, dpi=500)
     plt.close(fig)
 
 
+def plot_input_raster(spike_times, spiking_ys, rate, xlim, ylim,
+                      stim_times, stims, stim_dur, pattern_grps, grp_pos, fig_dir):
+    """Raster, firing rate, and stimulus train"""
+    # raster
+    fig = plt.figure(figsize=(8, 0.5))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_facecolor((0.95, 0.95, 0.95))
+    ax.scatter(spike_times, spiking_ys, c="black", marker='.', s=3, edgecolor="none")
+    ax2 = ax.twinx()
+    ax2.plot(np.linspace(xlim[0], xlim[1], len(rate)), rate, "black")
+    for t, stim in zip(stim_times, stims):
+        ax.plot([t, t + stim_dur], [ylim[-1] + 5, ylim[-1] + 5], PATTERN_COLORS[stim], lw=3)
+        ax.text(t + stim_dur / 2, ylim[-1] + 12, stim, ha="center", va="bottom", fontsize=7, weight="bold")
+    ax.set_xlim(xlim)
+    ax.set_xticks([])
+    ax.set_ylim(bottom=ylim[0])
+    ax.set_yticks([])
+    ax2.set_ylim(bottom=0)
+    sns.despine(right=False)
+    fig.savefig(fig_dir + "BBP_input_raster.png", bbox_inches="tight", transparent=True, dpi=500)
+    plt.close(fig)
+    # input fiber(bundle) locations
+    for i, pattern in enumerate(PATTERNS):
+        fig = plt.figure(figsize=(0.8, 0.8))
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(grp_pos[:, 0], grp_pos[:, 2], c="darkgrey", marker='.', s=20, edgecolor="none")
+        ax.scatter(grp_pos[pattern_grps[i], 0], grp_pos[pattern_grps[i], 2], c=PATTERN_COLORS[pattern],
+                   marker='.', s=40, edgecolor="none")
+        ax.text(np.min(plt.xlim()), np.max(plt.ylim()), " %s" % pattern, color=PATTERN_COLORS[pattern],
+                 ha="left", va="top", fontsize=7, weight="bold")
+        ax.axis("equal")
+        ax.axis("off")
+        fig.savefig(fig_dir + "pattern_%s.png" % pattern, bbox_inches="tight", transparent=True, dpi=500)
+        plt.close(fig)
+
+
 if __name__ == "__main__":
     t_start, t_end = 19200, 21200
     sim = Simulation(os.path.join(BBP_FN_DATA_DIR, "0", "BlueConfig"))
-    plot_raster(*setup_raster(sim, t_start, t_end), [t_start, t_end], "figs/paper/BBP_raster.png")
+    plot_raster(*setup_raster(sim, t_start, t_end), "figs/paper/BBP_raster.png")
+    plot_input_raster(*setup_input_raster(sim, t_start, t_end), "figs/paper/")
 
 
 
