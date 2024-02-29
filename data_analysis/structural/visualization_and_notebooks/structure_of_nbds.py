ae0023fadee59838a91c433da2844681d83cde4a
Ecker András <andras.ecker@epfl.ch>
2024-02-29 12:32:01 +0100

    new feature added

--- data_analysis/structural/visualization_and_notebooks/structure_of_nbds.py
+++ data_analysis/structural/visualization_and_notebooks/structure_of_nbds.py
@@ -1,6 +1,6 @@
 """
 Plotting basic features of neighbourhoods
-author: András Ecker, last update: 01.2024
+author: András Ecker, last update: 02.2024
 """
 
 import os
@@ -17,10 +17,13 @@ sns.set(style="ticks", context="paper", font="Helvetica Neue",
 DATA_DIR = "/gpfs/bbp.cscs.ch/home/egassant/reliability_and_structure/data_analysis/data"
 CONNECTOMES = ["BBP", "MICrONS", "Celegans", "Drosophila"]
 GRAPHS = ["original", "configuration_model", "ER_shuffle", "run_DD2"]
+GRAPHS_V2 = ["original", "ER", "distance"]
 cmap = plt.cm.Dark2
 PALETTE = {"original": "indianred",  # cmap(3),
            "configuration_model": cmap(2),
+           "ER": cmap(0),
            "ER_shuffle": cmap(0),
+           "distance": cmap(1),
            "run_DD2": cmap(1)}
 
 
@@ -36,6 +39,21 @@ def load_data():
             df["2_simplices_norm"] = df["2_simplices"] / df["1_simplices"]
             df["graph"] = graph
             dfs.append(df[["graph", "density", "rc_over_nodes", "0_simplices", "2_simplices_norm"]])
+        conn_data[conn] = pd.concat(dfs, ignore_index=True)
+    return conn_data
+
+
+def load_data_v2():
+    """Load data from saved DataFrames and concatenates them (across controls)"""
+    conn_data = {}
+    for conn in CONNECTOMES:
+        dfs = []
+        for graph in GRAPHS_V2:
+            if graph == "distance" and conn in ["Celegans", "Drosophila"]:
+                continue
+            df = pd.read_pickle(os.path.join(DATA_DIR, "props_%s_%s.pkl" % (conn, graph)))
+            df["graph"] = graph
+            dfs.append(df[["graph", "wasserstein_deg_total"]])
         conn_data[conn] = pd.concat(dfs, ignore_index=True)
     return conn_data
 
@@ -62,6 +80,8 @@ if __name__ == "__main__":
     for feature in ["0_simplices", "density", "rc_over_nodes", "2_simplices_norm"]:
         ext = "png" if feature in ["0_simplices", "density"] else "pdf"
         plot_feature(conn_data, feature, "figs/%s.%s" % (feature, ext))
+    conn_data = load_data_v2()
+    plot_feature(conn_data, "wasserstein_deg_total", "figs/wasserstein_deg_total.pdf")
 
 
 
