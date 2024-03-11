d7e6eef20614c63ea0302c432b62d53c4cc00479
Egas Santander Daniela <egassant@bbpv2.epfl.ch>
2024-03-11 14:59:03 +0100

    adding loaders for TriDy controls

--- classification/visualization/loaders.py
+++ classification/visualization/loaders.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 import itertools
+import conntility
 
 #### Getting size of union of selected neighborhoods
 def size_of_union_single(centers, df_nbds, return_nodes=False):
@@ -17,11 +18,11 @@ def size_of_union_single(centers, df_nbds, return_nodes=False):
     else:
         return union.shape[0]
     
-def load_centers_and_size_union(df_nbds, fname):
-    df_centers=pd.read_pickle(fname)
+def load_centers_and_size_union(df_nbds, df_centers):
+    #df_centers=pd.read_pickle(fname)
     union_sizes=[]
     for i in range(len(df_centers)):
-        c=df_centers.iloc[i, 3:].to_numpy()
+        c=df_centers.iloc[i].loc[[f"chief{k}" for k in range(50)]].to_numpy()
         union_sizes.append(size_of_union_single(c, df_nbds))
     df_centers["size_of_union"]=union_sizes
     return df_centers
@@ -130,7 +131,8 @@ def get_plot_df_random_PCA(data, df_chiefs_random, connectome, df_nbds):
     return plot_me_random   
 
 def reformat_PCA_results(fname_centers_structured, fname_centers_random, fname_acc, df_nbds, connectome): 
-    df_chiefs_structured=load_centers_and_size_union(df_nbds, fname_centers_structured)
+    df_chiefs_structured=pd.read_pickle(fname_centers_structured)
+    df_chiefs_structured=load_centers_and_size_union(df_nbds, df_chiefs_structured)
     df_chiefs_random=pd.read_pickle(fname_centers_random)
     data=load_and_stats_PCA(fname_acc)
     return (get_plot_df_structured_PCA(data, df_chiefs_structured),
@@ -138,8 +140,7 @@ def reformat_PCA_results(fname_centers_structured, fname_centers_random, fname_a
     
 
 #### Formatting TriDy data 
-def load_acc_tridy(prefix, df_centers):
-    fparams_short2long = {# Featurization paramters
+fparams_short2long = {# Featurization paramters
         'asg':'asg', 'asl':'asl', 'asr':'asr',
         'blsg':'blsg', 'blsl':'blsg_low', 'blsr':'blsg_radius',
         'blsgR':'blsg_reversed', 'blsRr':'blsg_reversed_radius', 'blsRl':'blsg_reversed_low',
@@ -152,6 +153,7 @@ def load_acc_tridy(prefix, df_centers):
         #'rc':'rc', 'rcc':'rc_chief'
         
     }
+def load_acc_tridy(prefix, df_centers):
     params = np.unique(df_centers['first_selection'].values)
     params = np.array([p for p in params if p not in ['rc_per_edges','rc_per_nodes']])
     results= {}
@@ -188,3 +190,51 @@ def get_plot_df_structured_tridy(df_centers, df_acc):
 
     return pd.concat([relabel_df(df_single, double_classification=False),
                       relabel_df(df_double, double_classification=True)], axis=1)
+
+def get_plot_df_controls_tridy(df_nbds, conn_dir="../../data_analysis/data/"):
+    def concat_selection_and_acc(df_centers, df_acc):
+        return pd.concat([df_centers.loc[:,["selection", "size_of_union"]], 
+                          df_acc.set_index("bin_number").loc[df_centers.index]], axis=1)
+
+    "Get data for controls.  All paths are hardcoded, because they all have different data formats"
+    ctrs={}
+    for era, label in [(6, "_sparse"), (7, "_dense"), (5, "")]: 
+        # Load selection and add neighborhood sizes
+        if label !="":
+            fname=f"/gpfs/bbp.cscs.ch/project/proj102/lazovski/2022-12-15-ee-subcircuit/parameters/selections-era{era}.pkl" 
+            df_centers=pd.read_pickle(fname).query("selection in ['random0', 'random1', 'random2', 'random3']")
+            df_centers=load_centers_and_size_union(df_nbds, df_centers)
+        else: #fully random selection (structure is different for this data)
+            # Load full connectome to re-index data 
+            conn_full=conntility.ConnectivityMatrix.from_h5(f'{conn_dir}/connectome_BBP_V5.h5')
+            temp=conn_full.vertices
+            temp["indx_EE"]=np.nan
+            temp["indx_EE"][temp["synapse_class"]=='EXC']=np.arange((temp["synapse_class"]=='EXC').sum())
+            # Load selection
+            fname=f"/gpfs/bbp.cscs.ch/project/proj102/lazovski/2022-12-15-ee-subcircuit/parameters/selections_exc.pkl"
+            df_centers=pd.read_pickle(fname).iloc[range(100)]# 100 random samples of size 50 from the E-E subcircuit
+            sel=df_centers["selection"]
+            df_centers=df_centers.drop("selection", axis=1) # 100 random samples of size 50 from the E-E subcircuit
+            # Re-index
+            df_centers=pd.DataFrame(temp.iloc[df_centers.to_numpy().flatten()]["indx_EE"].to_numpy().reshape(df_centers.shape), 
+                            index=df_centers.index, 
+                            columns=df_centers.columns).astype(int)
+            df_centers["selection"]=sel
+            # Add neibhborhood sizes within the E-E subcircuit 
+            df_centers=load_centers_and_size_union(df_nbds, df_centers)      
+    
+        # Add activity data
+        if era!=5:
+            root = '/gpfs/bbp.cscs.ch/project/proj102/lazovski/2024-01-10-rcpn-redo/TriDy-tools/dataframes/'
+        if era==5:
+            root=f"/gpfs/bbp.cscs.ch/home/lazovski/TriDy-tools/dataframes/"
+        prefix=f"{root}era{era}-"
+        ctrs[label]={}
+        for p in fparams_short2long:
+            df_acc=pd.read_pickle(f"{prefix}{p}.pkl").sort_values(by='bin_number').reset_index(drop=True)
+            temp=concat_selection_and_acc(df_centers, df_acc)
+            temp["control_type"]=f"random{label}"
+            temp["control_number"]=np.array([x[6:] for x in temp["selection"]]).astype(int)
+            ctrs[label][p]=temp.set_index(["control_type", "control_number"])
+    # Group by parameter for plotting
+    return {p: pd.concat([ctrs[key][p] for key in ctrs.keys()], axis=0) for p in fparams_short2long}
