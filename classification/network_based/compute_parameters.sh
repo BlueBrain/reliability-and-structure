ce11b385441833e6752d1e9c0ece6a83ab5eeadc
jlazovskis <jlazovskis@gmail.com>
2024-03-12 23:54:11 +0200

    del sh file, minor

--- classification/network_based/compute_parameters.sh
+++
@@ -1,8 +0,0 @@
-python compute_parameters.py spectral
-python compute_parameters.py spectral_reverse
-python compute_parameters.py simplices
-python compute_parameters.py degrees
-python compute_parameters.py cc
-python compute_parameters.py dc
-python compute_parameters.py nbc
-python compute_parameters.py rc
