6a8fc16badc03b97474541ed63df2a85d2c60d14
Ecker András <andras.ecker@epfl.ch>
2024-03-26 16:46:35 +0100

    new RC sims added

--- data_analysis/activity/visualization/rc_ctrls.py
+++ data_analysis/activity/visualization/rc_ctrls.py
@@ -4,9 +4,11 @@ author: András Ecker, last update: 03.2024
 """
 
 import os
+from itertools import combinations
 import numpy as np
 import pandas as pd
 from scipy.stats import kruskal
+from conntility import ConnectivityMatrix
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -27,29 +29,43 @@ SIMS = {"baseline": "BlobStimReliability_O1v5-SONATA_Baseline",
         "RC - 4": "BlobStimReliability_O1v5-SONATA_RecipRemoval_Unstruct-3",  # not possible to give it a pair
         "RC + 1": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x2",
         "RC + 1 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x2",
-        "RC + 2": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x4",
-        "RC + 2 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x4",
-        "RC + 3": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x8",
-        "RC + 3 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x8",
-        "RC + 4": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x16",
-        "RC + 4 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x16"}
-COLS_REMOVE, COLS_ADD = plt.colormaps["summer_r"](np.linspace(0.2, 0.9, 4)), plt.cm.RdPu(np.linspace(0.2, 0.9, 4))
+        "RC + 2": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x3",
+        "RC + 2 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x3",
+        "RC + 3": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x4",
+        "RC + 3 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x4",
+        "RC + 4": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x5",
+        "RC + 4 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x5",
+        "RC + 5": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x8",
+        "RC + 5 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x8"}
+        # "RC + 6": "BlobStimReliability_O1v5-SONATA_ConnAdd_RecipStruct0x16",
+        # "RC + 6 (ctrl)": "BlobStimReliability_O1v5-SONATA_ConnAdd_Control0x16"}
+COLS_REMOVE = plt.colormaps["summer_r"](np.linspace(0.2, 0.9, 4))
+COLS_ADD = plt.cm.RdPu(np.concatenate([np.linspace(0.2, 0.9, 4), np.array([1.])]))
 CMAP = {"RC - 1": COLS_REMOVE[0], "RC - 2": COLS_REMOVE[1],  "RC - 3": COLS_REMOVE[2], "RC - 4": COLS_REMOVE[3],
-        "RC + 1": COLS_ADD[0], "RC + 2": COLS_ADD[1], "RC + 3": COLS_ADD[2], "RC + 4": COLS_ADD[3]}
-
+        "RC + 1": COLS_ADD[0], "RC + 2": COLS_ADD[1], "RC + 3": COLS_ADD[2], "RC + 4": COLS_ADD[3],
+        "RC + 5": COLS_ADD[4]}
 
 
 def load_reliabilities():
-    rels = {mod_name: np.load(os.path.join(SIM_DIR, sim_name, "working_dir", "reliability_basic.npz"))["reliability"]
-            for mod_name, sim_name in SIMS.items()}
-    return pd.DataFrame(rels)
+    return pd.DataFrame({mod_name: np.load(os.path.join(SIM_DIR, sim_name, "working_dir",
+                                                        "reliability_basic.npz"))["reliability"]
+                         for mod_name, sim_name in SIMS.items()})
 
 
 def load_simplex_counts():
-    scs = {mod_name: pd.read_pickle(os.path.join(SIM_DIR, sim_name,
-                                                 "working_dir", "simplex_counts.pkl"))["simplex_counts_EXC"]
-           for mod_name, sim_name in SIMS.items()}
-    return pd.DataFrame(scs)
+    return pd.DataFrame({mod_name: pd.read_pickle(os.path.join(SIM_DIR, sim_name, "working_dir",
+                                                               "simplex_counts.pkl"))["simplex_counts_EXC"]
+                         for mod_name, sim_name in SIMS.items()})
+
+
+def get_edges_diff():
+    """Gets difference in edges from saved connectivity matrices"""
+    conn_mats = {mod_name: ConnectivityMatrix.from_h5(os.path.join(SIM_DIR, sim_name, "working_dir",
+                                              "connectome.h5")).index("synapse_class").isin("EXC").matrix.astype(int)
+                 for mod_name, sim_name in SIMS.items()}
+    rel_edges = pd.Series({mod_name: (conn_mat - conn_mats["baseline"]).nnz / 2 / conn_mats["baseline"].nnz * 100
+                          for mod_name, conn_mat in conn_mats.items()})
+    return rel_edges.drop("baseline")
 
 
 def plot_rels(df, x, fig_name):
@@ -83,22 +99,41 @@ def plot_scounts(df, x, fig_name):
     plt.close(fig)
 
 
-def plot_rel_means(rel_means, pvals, fig_name, sign_y=0.1):
+def plot_pvals(pvals, fig_name):
+    """Plot p-value matrix"""
+    fig = plt.figure(figsize=(1.8, 1.6))
+    ax = fig.add_subplot(1, 1, 1)
+    i = ax.imshow(pvals, cmap="viridis", interpolation="nearest", aspect="auto")
+    fig.colorbar(i)
+    ax.set_xticks(np.arange(len(pvals.columns)))
+    ax.set_xticklabels(pvals.columns.to_numpy(), fontsize=5, rotation=90)
+    ax.set_yticks(np.arange(len(pvals.index)))
+    ax.set_yticklabels(pvals.index.to_numpy(), fontsize=5)
+    fig.savefig(fig_name, bbox_inches="tight", transparent=True)
+    plt.close(fig)
+
+
+def plot_rel_means(rel_means, pvals, fig_name, sign_y=0.07):
     """Plots means relative to baseline"""
-    colors, edge_colors, sign = [], [], []
+    colors, hatches, sign = [], [], []
     for i, mod_name in enumerate(rel_means.index.to_numpy()):
         if " (ctrl)" in mod_name:
-            colors.append("white")
-            edge_colors.append(CMAP[mod_name.replace(" (ctrl)", "")])
+            colors.append(CMAP[mod_name.replace(" (ctrl)", "")])
+            hatches.append('//')
         else:
             colors.append(CMAP[mod_name])
-            edge_colors.append(CMAP[mod_name])
-        if pvals[mod_name] < 0.05:
+            if mod_name != "RC - 4":
+                hatches.append('oo')
+            else:
+                hatches.append('oo//')
+        if pvals.loc["baseline", mod_name] < 0.05:
             sign.append(i)
 
-    fig = plt.figure(figsize=(4.5, 1.6))
+    fig = plt.figure(figsize=(1.8, 1.6))
     ax = fig.add_subplot(1, 1, 1)
-    ax.bar(np.arange(len(rel_means)), rel_means, color=colors, edgecolor=edge_colors)
+    bars = ax.bar(np.arange(len(rel_means)), rel_means, color="white", edgecolor=colors)
+    for bar, hatch in zip(bars, hatches):
+        bar.set_hatch(hatch)
     ax.plot([-1, len(rel_means)], [0, 0], color="black", ls="dotted", lw=0.75)
     for x in sign:
         ax.text(x, sign_y, "*", ha="center", fontsize=9)
@@ -110,25 +145,21 @@ def plot_rel_means(rel_means, pvals, fig_name, sign_y=0.1):
 
 def plot_scatter(rel_edges, rel_means, fig_name):
     """Plots means vs. pct. of edges changed"""
-    rem = ["RC - %i" %i for i in range(1, 5)]
-    rem_ctrl = ["RC - %i (ctrl)" %i for i in range(1, 4)]
-    add = ["RC + %i" %i for i in range(1, 5)]
-    add_ctrl = ["RC + %i (ctrl)" % i for i in range(1, 5)]
-    colors = []
+    colors, mod, mod_ctrl = [], [], []
     for mod_name in rel_means.index.to_numpy():
         if " (ctrl)" in mod_name:
             colors.append(CMAP[mod_name.replace(" (ctrl)", "")])
+            mod_ctrl.append(mod_name)
         else:
             colors.append(CMAP[mod_name])
+            mod.append(mod_name)
 
     fig = plt.figure(figsize=(1., 0.8))
     ax = fig.add_subplot(1, 1, 1)
-    ax.plot(rel_edges[rem].to_numpy(), np.abs(rel_means[rem].to_numpy()), c=CMAP["RC - 2"])
-    ax.plot(rel_edges[rem_ctrl].to_numpy(), np.abs(rel_means[rem_ctrl].to_numpy()), c=CMAP["RC - 2"], ls="--")
-    ax.plot(rel_edges[add].to_numpy(), np.abs(rel_means[add].to_numpy()), c=CMAP["RC + 2"])
-    ax.plot(rel_edges[add_ctrl].to_numpy(), np.abs(rel_means[add_ctrl].to_numpy()), c=CMAP["RC + 2"], ls="--")
-    ax.scatter(rel_edges.to_numpy(), np.abs(rel_means.to_numpy()), s=10, c=colors)
-    sns.despine(trim=True, offset=2)
+    ax.plot(rel_edges[mod].to_numpy(), rel_means[mod].to_numpy(), c=colors[int(len(colors) / 2)])
+    ax.plot(rel_edges[mod_ctrl].to_numpy(), rel_means[mod_ctrl].to_numpy(), c="gray", ls="--")
+    ax.scatter(rel_edges.to_numpy(), rel_means.to_numpy(), s=10, c=colors)
+    sns.despine(trim=True, offset=1)
     fig.savefig(fig_name, bbox_inches="tight", transparent=True)
     plt.close(fig)
 
@@ -136,17 +167,25 @@ def plot_scatter(rel_edges, rel_means, fig_name):
 if __name__ == "__main__":
     rels = load_reliabilities()
     scs = load_simplex_counts()
-    for x in ["RC - %i" % i for i in range(1, 5)] + ["RC + %i" % i for i in range(1, 5)]:
+    for x in ["RC - %i" % i for i in range(1, 5)] + ["RC + %i" % i for i in range(1, 6)]:
         plot_rels(rels, x, "figs/paper/%s_reliability.pdf" % x.replace(" ", ""))
         plot_scounts(scs, x, "figs/paper/%s_simplex_counts.pdf" % x.replace(" ", ""))
+
+    mod_names = rels.columns
+    data = np.full((len(mod_names), len(mod_names)), np.nan, dtype=np.float32)
+    row_idx, col_idx = np.triu_indices(len(mod_names), k=1)
+    for i, j in zip(row_idx, col_idx):
+        data[i, j] = kruskal(rels[mod_names[i]], rels[mod_names[j]], nan_policy="omit").pvalue
+    pvals = pd.DataFrame(data, index=mod_names, columns=mod_names)
+    plot_pvals(pvals.iloc[1:, 1:], "figs/paper/rel_pvals.pdf")
     means = rels.mean()
     rel_means = means.drop("baseline") - means["baseline"]
-    pvals = {mod_name: kruskal(rels["baseline"].to_numpy(), rels[mod_name].to_numpy(), nan_policy="omit").pvalue
-             for mod_name in rel_means.index.to_numpy()}
-    plot_rel_means(rel_means, pvals, "figs/paper/rel_means.pdf")
-    n_edges = scs.loc[1]
-    rel_edges = np.abs(1 - n_edges.drop("baseline") / n_edges["baseline"]) * 100
-    plot_scatter(rel_edges, rel_means, "figs/paper/rel_means_vs_rel_edges.pdf")
+    mod_names = list(SIMS.keys())
+    plot_rel_means(rel_means.iloc[:7], pvals, "figs/paper/rel_means-.pdf", sign_y=0.0001)
+    plot_rel_means(rel_means.iloc[7:], pvals, "figs/paper/rel_means+.pdf")
+    rel_edges = get_edges_diff()
+    plot_scatter(rel_edges.iloc[:7], rel_means.iloc[:7], "figs/paper/rel_means_vs_rel_edges-.pdf")
+    plot_scatter(rel_edges.iloc[7:], rel_means.iloc[7:], "figs/paper/rel_means_vs_rel_edges+.pdf")
 
 
 
