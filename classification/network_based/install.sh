343ba8a55eac16d0f4d7225ac737af48742294a8
Jānis Lazovskis <jlazovskis@gmail.com>
2024-04-08 10:45:17 +0300

    install tridy

--- classification/network_based/install.sh
+++ classification/network_based/install.sh
@@ -3,5 +3,11 @@
 # Install required packages
 pip install -r requirements.txt
 
-# Install TriDy
-cd ./TriDy && 
+# Download spike trains for TriDy
+cd ./TriDy
+curl -O https://zenodo.org/record/4290212/files/input_data.zip
+unzip input_data.zip 
+for f in $(ls input_data/); do mv ${f} data; done
+
+# Extract data 
+cd data && python extract_data.py
