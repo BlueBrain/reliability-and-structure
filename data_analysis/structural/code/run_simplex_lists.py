78fcd3198b74b4205d6e38f50227b8f9be69fbc3
Egas Santander Daniela <egassant@bbpv2.epfl.ch>
2024-04-16 15:23:43 +0200

    Adding scripts for simplex list for zenodo data

--- data_analysis/structural/code/run_simplex_lists.py
+++ data_analysis/structural/code/run_simplex_lists.py
@@ -1,12 +1,13 @@
 '''
-Compute simplex lists for all connectomes
+Compute simplex lists for all connectomes and their controls
 Author(s): Daniela Egas S.
-Last updated: 01.2024
+Last updated: 04.2024
 '''
 
 import sys
 sys.path.append('../../../library')
 from structural_basic import *
+
 
 def main():
     for conn in ['Celegans', 'Drosophila', 'MICrONS', 'BBP']:
@@ -18,8 +19,19 @@ def main():
             'analyses': {
                 'list_simplices_by_dimension': {
                     'save_suffix': '',
-                    'kwargs': {'threads': 10}        
-                }}}
+                    'kwargs': {
+                        'threads': 10,
+                        'max_simplices':False,
+                    },
+                    "controls":{
+                        "seeds":[10],
+                        "types":{
+                            "configuration_model":{},
+                            "ER_model":{}
+                        }
+                    }
+                }
+            }}
     
         print(f'Computing {cfg["connectome"]["name"]}')
         compute_basic_props(cfg)
